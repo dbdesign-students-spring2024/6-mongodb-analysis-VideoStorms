fb2dc7bd3e67c1617be7aa10a2843953f5690a25
“Rares <raresh.grecu@gmail.com>
2024-04-03 13:16:32 -0400

    Fixed cleanupdata

--- cleanupdata.py
+++ cleanupdata.py
@@ -6,12 +6,19 @@ data = pd.read_csv('listings.csv')
 data['name'] = data['name'].str.title()
 data['neighbourhood'] = data['neighbourhood'].str.title()
 
-# Remove unrealistic prices
+# Remove dollar signs and commas, then convert to float
+data['price'] = data['price'].str.replace('$', '').str.replace(',', '').astype(float)
+
+# Perform the filtering
 data = data[(data['price'] > 0) & (data['price'] <= 10000)]
+
+# When adding the dollar sign back, consider formatting the float to two decimal places
+data['price'] = '$' + data['price'].round(2).astype(str)
 
 # Fill missing review scores with the average score
 average_score = data['review_scores_rating'].mean()
-data['review_scores_rating'].fillna(average_score, inplace=True)
+data['review_scores_rating'] = data['review_scores_rating'].fillna(average_score)
+
 
 # Save the cleaned data
 data.to_csv('listings_clean.csv', index=False)
